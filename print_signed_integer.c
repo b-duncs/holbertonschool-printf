4e04613f6ea2f058a0902ddc3655cc132c2fe818
NVOzmus <nvozmus@outlook.com>
2022-03-01 12:25:55 -0800

    added template and notes for print_unsigned_integer.c and print_signed_integer.c

---
+++ print_signed_integer.c
@@ -0,0 +1,4 @@
+/*  Pseudo code below, to be corrected. Inside of this file will include at least one function to print a signed (d) integer.
+ *  Code must be able to run at min/max int size.
+ *  Need to update prototype to main.h when settled on function
+ */
---
+++ print_unsigned_integer.c
@@ -0,0 +1,9 @@
+/**
+ * Pseudo code and to-do below.
+ * Files must be able to run at min/max ints, to account for any errors
+ * To-Do:
+ * - add prototype to main.h
+ * - Everything
+ */
+
+
